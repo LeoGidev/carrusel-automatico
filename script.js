f33b8c73f034a8a73382af8c4c924084c79fc60b
LeoGidev <leogimenez.dev@gmail.com>
2024-01-11 15:16:40 -0300

    se trabaja en js

--- script.js
+++ script.js
@@ -1,20 +1,44 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const slide = document.querySelector('.carousel-slide');
     const images = document.querySelectorAll('.carousel-image');
+    const slideWidth = 300; // Ancho de cada imagen a mover
+    const moveDistance = -300; // Distancia para mover la imagen
+    const hidePosition = 600; // Posición para ocultar la imagen
+    
+    let currentIndex = 0; // Indice actual
   
-    let currentIndex = 0; // Indice actual del carrusel
-    const slideWidth = 330; // Ancho de cada imagen + margen
+    // Función para mover una imagen
+    function moveImage(index) {
+      //const image = images[index];
   
-    // Función para mover el carrusel
-    function moveCarousel() {
-      currentIndex = (currentIndex + 1) % images.length; // Actualiza el índice
-      const newPosition = -currentIndex * slideWidth; // Calcula la nueva posición
+      // Paso 1: Mover la imagen -300px con transición suave
+      images[index].style.transition = 'transform 2s ease-in-out';
+      setTimeout(() => {
+      images[index].style.transform = `translateX(${moveDistance}px)`;
+      },1000);
+      // Paso 2: Ocultar la imagen
+      setTimeout(() => {
+        images[index].style.opacity = '0';
+      }, 1000); // Después de 1 segundo (tiempo de transición)
   
-      slide.style.transform = `translateX(${newPosition}px)`; // Aplica la nueva posición
+      // Paso 3: Ubicar la imagen a +600px y hacerla visible
+      setTimeout(() => {
+        images[index].style.transform = `translateX(${hidePosition}px)`;
+        
+      }, 2000); // Después de 2 segundos (tiempo de transición + 1 segundo)
+  
+      // Paso 4: Mover la imagen a -300 con transición suave y comenzar el bucle nuevamente
+      setTimeout(() => {
+        images[index].style.opacity = '1';
+        //image.style.transition = 'none'; // Quita la transición para mover instantáneamente
+        images[index].style.transform = `translateX(${moveDistance}px)`;
+        currentIndex = (currentIndex + 1) % images.length; // Actualiza el índice para la siguiente imagen
+        setTimeout(() => moveImage(currentIndex), 1000); // Llama a la función de nuevo después de 1 segundo
+      }, 3000); // Después de 3 segundos (tiempo total)
     }
   
-    // Configuración del intervalo para mover automáticamente el carrusel
-    setInterval(moveCarousel, 2000); // Mueve cada 2 segundos
+    // Iniciar el bucle con la primera imagen
+    moveImage(currentIndex);
   });
+  
   
   
