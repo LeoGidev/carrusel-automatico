4a0bf46109ad645de8e74b7c818568f126ced732
LeoGidev <leogimenez.dev@gmail.com>
2024-01-11 15:43:25 -0300

    se trabaja en js

--- script.js
+++ script.js
@@ -5,15 +5,19 @@ document.addEventListener("DOMContentLoaded", function() {
     const hidePosition = 600; // Posición para ocultar la imagen
     
     let currentIndex = 0; // Indice actual
+    
   
     // Función para mover una imagen
     function moveImage(index) {
-      //const image = images[index];
-  
-      // Paso 1: Mover la imagen -300px con transición suave
+      
+    // Paso 1: Mover la imagen -300px con transición suave
       images[index].style.transition = 'transform 2s ease-in-out';
       setTimeout(() => {
       images[index].style.transform = `translateX(${moveDistance}px)`;
+      console.log(index);
+      images[index+1].style.transform=`translateX(${moveDistance}px)`;
+      images[index+2].style.transform=`translateX(${moveDistance}px)`;
+
       },1000);
       // Paso 2: Ocultar la imagen
       setTimeout(() => {
