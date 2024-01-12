204563f89353b055e8a938257af6a569035792f3
LeoGidev <leogimenez.dev@gmail.com>
2024-01-12 01:37:14 -0300

    se trabaja en js

--- script.js
+++ script.js
@@ -3,14 +3,27 @@ document.addEventListener("DOMContentLoaded", function() {
     const imag = document.querySelectorAll('.carousel-image');
     let distancia = -330; // ancho de moviemiento
     let indiceactual = 0; // Indice actual
-    let vuelta = 0;
+    let vuelta = 930;
+    let a = 0;
+
+    for(let i=0;i<imag.length;i++){
+        if (i<3){
+        mov = 930;
+        }else{mov = -330;}
+        imag[i].style.transform = `translateX(${mov}px)`;
+        a = a +1;
+
+
+    }
   
     
-
+  
   
     
     // Función para mover una imagen
     function moveImage(index) {
+        
+         
         
        
       
@@ -23,21 +36,21 @@ document.addEventListener("DOMContentLoaded", function() {
      
 
         function ocultar(){
-            imag[index].style.opacity='0';
+            imag[index].style.opacity='1';
         }
         setTimeout(ocultar,4000);
       
    
         function volverainicio(){
-            vuelta = (distancia*-1);
+            //vuelta = (distancia*-1);
 
             imag[index].style.transform = `translateX(${vuelta}px)`;
         }
         setTimeout(volverainicio, 5000);
 
-        distancia=distancia - 330;
+        
         indiceactual = (indiceactual + 1) % imag.length; // Actualiza el índice para la siguiente imagen
-      
+        //distancia= -330*(indiceactual+1);
     }
   
     // Iniciar el bucle con la primera imagen
