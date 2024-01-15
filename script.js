176d88733144576293d181bc795e6050ed47ed81
LeoGidev <leogimenez.dev@gmail.com>
2024-01-15 18:23:21 -0300

    se trabaja en carrusel

--- script.js
+++ script.js
@@ -1,88 +1,60 @@
 document.addEventListener("DOMContentLoaded", function() {
-    
     const imag = document.querySelectorAll('.carousel-image');
-    let distancia = -330; // ancho de moviemiento
-    let indiceactual = 0; // Indice actual
+    let distancia = -330; // Ancho de movimiento
+    let indiceactual = 0; // Índice actual
     let vuelta = 0;
-    let a = 0;
-    let c =0;
-    let coleros=[];
-    let newindex=0;
+    let coleros = [];
+    let newindex = 0;
 
-  
-   
-    
-  
-  
-    
     // Función para mover una imagen
-    function moveImage(index) {
-        
-         
-        
-       
-      
-     function muevete(){
-        let dist = distancia + (index*-330);
+    function moveImage(index, callback) {
+        let dist = distancia + (index * -330);
 
-        for(let e=0;e<imag.length;e++){
-            newindex = index+e;
-            
-            if (newindex < imag.length){
+        for (let e = 0; e < imag.length; e++) {
+            newindex = index + e;
 
-        imag[newindex].style.opacity='1';
-        imag[newindex].style.transform = `translateX(${dist}px)`; 
-               
+            if (newindex < imag.length) {
+                imag[newindex].style.opacity = '1';
+                imag[newindex].style.transform = `translateX(${dist}px)`;
             }
-            }
-            //console.log(coleros.length);
-            if(coleros.length!=0 && coleros.length<imag.length){
-                for(ind=0;ind<coleros.length;ind++){
-                    diferencia = (imag.length-coleros.length);
-                    //console.log("dif:", diferencia);
-                   let xx = (diferencia*330)-330;
-                   //console.log("distanciaxx: ", xx);
-                   //console.log("valor de ind:", ind, "valor del largo de coleros:", coleros.length, "valor de el largo de imag:", imag.length);
-                imag[ind].style.opacity='1';
-                imag[ind].style.transform = `translateX(${xx}px)`;
-                    }
-                    
+        }
 
-            }
-            
+        // Callback para indicar que la función moveImage ha terminado
+        callback();
     }
-    
 
-     
-     setTimeout(muevete, 2000);
-     
+    // Función para volver al inicio
+    function volverainicio(index) {
+        imag[index].style.opacity = '0';
+        vuelta = 330 * (imag.length - 1);
+        imag[index].style.transform = `translateX(${vuelta}px)`;
 
-       
-      
-   
-        function volverainicio(){
-            imag[index].style.opacity='0';
-           
-            vuelta = 330*(imag.length-1);
-            imag[index].style.transform = `translateX(${vuelta}px)`;
-            if(coleros.length<imag.length){
-            coleros.push(index);}
-            else{coleros=[];
-            coleros.push(index);}
-           
-            }
-        setTimeout(volverainicio, 6000);
-
-        
-        indiceactual = (indiceactual + 1) % imag.length; // Actualiza el índice para la siguiente imagen
-        
+        // Manejo de coleros
+        if (coleros.length < imag.length) {
+            coleros.push(index);
+        } else {
+            coleros = [index];
+        }
     }
-  
+
+    // Función para realizar la animación completa
+    function animacionCompleta(index) {
+        moveImage(index, function() {
+            setTimeout(function() {
+                volverainicio(index);
+            }, 4000);
+        });
+
+        // Actualiza el índice para la siguiente imagen
+        indiceactual = (indiceactual + 1) % imag.length;
+    }
+
     // Iniciar el bucle con la primera imagen
-    setInterval(() => {
-        moveImage(indiceactual);
-        },8000); 
-  });
+    setInterval(function() {
+        animacionCompleta(indiceactual);
+    }, 8000);
+});
+
   
   
   
